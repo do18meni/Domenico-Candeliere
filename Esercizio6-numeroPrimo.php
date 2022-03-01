fb870e8924b6e5b43bf19bf033d5f9c888777171
do18meni <domcandeliere@gmail.com>
2022-03-01 17:32:52 +0100

    add Excercise

--- Esercizio6-numeroPrimo.php
+++ Esercizio6-numeroPrimo.php
@@ -1,5 +1,12 @@
 <?php
-
+    $Numero =(int)readline('Inserisci numero: ');
+    
+    if($Numero%$Numero ===0 & $Numero/1){
+echo 'Il numero è primo';
+    }
+    else{
+echo 'non è un numero primo';
+    }
 
 
 ?>
